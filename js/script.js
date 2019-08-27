547ea483e755f98b76a54abd9236b5ac421d4e6d
apelsun5 <patyuta@apelsun.ua>
2019-08-27 14:53:21 +0300

    afterHWfunction

--- js/script.js
+++ js/script.js
@@ -72,19 +72,27 @@ chooseExpenses();
 //     }
 // } while (k < 2);
 
-appData.moneyPerDay = (appData.budget / 30).toFixed(2);
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed(2);
 
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-    console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('Высокий уровень достатка');
-} else {
-    console.log('Произошла ошибка');
+    alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 }
+
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log('Минимальный уровень достатка');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log('Средний уровень достатка');
+    } else if (appData.moneyPerDay > 2000) {
+        console.log('Высокий уровень достатка');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+detectLevel();
 
 function checkSavings() {
     if (appData.savings == true) {
@@ -97,5 +105,26 @@ function checkSavings() {
 }
 
 checkSavings();
+
+function chooseOptExpenses() {
+    for (let i = 0; i < 3; i++) {
+        let a = prompt('Статья необязательных расходов', '');
+        if ( (typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+            console.log('done');
+            appData.optionalExpenses[i] = a;
+            console.log(i);
+        } else {
+            console.log('not done');
+            if (i == 0) {
+                i = -1;
+            } else {
+                i--;
+            }
+            console.log(i);
+        }
+    }
+}
+
+chooseOptExpenses();
 
 console.log(appData);
